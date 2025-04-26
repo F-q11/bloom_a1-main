404376dd1b83332c35173257a8a2fa59cf895cf0
Habib <habibsaeed2023@gmail.com>
2025-04-26 15:32:08 +0300

    new updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,12 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+
+            isMinifyEnabled = true  // important to enable
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"  // <-- This line
+        )
         }
     }
 }
